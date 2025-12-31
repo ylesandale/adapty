d9e0ea3a4e0c9c691f31d464b708196f63644af7
ylesandale <ylesandale7@gmail.com>
2025-12-31 21:07:38 +0300

    fix: sanity deploy v2

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -2,8 +2,8 @@ import { createClient } from 'next-sanity'
 import imageUrlBuilder from '@sanity/image-url'
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'your-project-id',
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  projectId: 'c3kk9rei',
+  dataset: 'production',
   apiVersion: '2024-01-01',
   useCdn: process.env.NODE_ENV === 'production',
 })
@@ -14,4 +14,3 @@ const builder = imageUrlBuilder(client)
 export function urlFor(source: any) {
   return builder.image(source)
 }
-
